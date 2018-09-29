2f62bb5a031807b487a85aac54d2622c5678abb2
Sosmogyi <nexus.laci@gmail.com>
2018-09-29 13:41:10 +0200

    Add files

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+const secure = require('express-force-https')
 // create the express app
 const app = express()
+app.use(secure)
 // create middleware to handle the serving the app
 app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
+// Catch all routes and redirect to the index file
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
+})
 // Create default port to serve the app on
 const port = process.env.PORT || 5000
 app.listen(port)
-// Log a feedback that this is actually running
+// Log to feedback that this is actually running
 console.log('Server started on port ' + port)
